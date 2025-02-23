bd0d1827b1544ffe815c369faee39fe00b1ad4eb
Hryhorii Fil <filgrigorii@gmail.com>
2025-02-23 13:12:01 +0200

    fix

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import 'izitoast/dist/css/iziToast.min.css';
 
 const form = document.querySelector('#search-form');
 const input = document.querySelector('#search');
+const gallery = document.querySelector('.gallery');
 const errorMessage = document.querySelector('#error-message');
 
 form.addEventListener('submit', async (event) => {
@@ -19,8 +20,10 @@ form.addEventListener('submit', async (event) => {
         return;
     }
 
-    showLoader();
+    gallery.innerHTML = '';
     errorMessage.style.display = 'none';
+
+    showLoader();
 
     try {
         const images = await fetchImages(query);
@@ -34,6 +37,8 @@ form.addEventListener('submit', async (event) => {
     } catch (error) {
         hideLoader();
         console.error('Error fetching images:', error);
+
+        showError('Failed to fetch images. Please check your request or try again later.');
         iziToast.error({
             title: 'Error',
             message: 'Failed to fetch images. Please try again later.'
